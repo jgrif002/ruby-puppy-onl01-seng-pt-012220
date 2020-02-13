a554f7c81258ffb8b24d38bf1d69e5cb8b23c496
Jesse Griffin <jesse.griffin@gmail.com>
2020-02-13 01:24:18 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,17 @@
 # Add your code here
+class Dog 
+  
+  attr_accessor  :name 
+  @@names = []
+  
+  
+  def initialize(name) 
+    @name = name 
+    @@names << name 
+  end 
+  
+  def Self.all 
+    @@names 
+  end 
+  
+end 
